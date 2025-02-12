93cdac43da696528623739469829dcb98b64bdf7
LuigiTime34 <lnorvell255@protonmail.com>
2025-02-11 21:08:09 -0500

    Changed password visibilit 😅

--- admin_start.py
+++ admin_start.py
@@ -3,6 +3,6 @@ from werkzeug.security import generate_password_hash
 
 with app.app_context():
     db.create_all()
-    admin = User(username='admin', password_hash=generate_password_hash('hinge255'))
+    admin = User(username='admin', password_hash=generate_password_hash('ha you thought I was going to share my password'))
     db.session.add(admin)
     db.session.commit()
